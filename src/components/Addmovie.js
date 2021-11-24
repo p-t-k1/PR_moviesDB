f7112cd942faa68b745223a807f51016618ae99e
Patryk Przybyło <patryk13342@o2.pl>
2021-11-24 16:58:21 +0100

    first commit

---
+++ src/components/Addmovie.js
@@ -0,0 +1,13 @@
+import logo from '../logo.svg';
+import '../App.css';
+import '../styles.css';
+
+function Login() {
+  return (
+      <div id="bodyBackground">
+        Login
+      </div>
+  );
+}
+
+export default Login;
---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import logo from '../logo.svg';
+import '../App.css';
+import '../styles.css';
+
+function Homepage() {
+  return (
+      <div id="bodyBackground">
+        Homepage
+      </div>
+  );
+}
+
+export default Homepage;
